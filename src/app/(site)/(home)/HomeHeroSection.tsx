e827e777099f96db4228f22c67eb8c4a2dd49ec5
trishane <trishanereid@gmail.com>
2024-09-27 09:49:08 +0530

    created basic structure of hero section

--- src/app/(site)/(home)/HomeHeroSection.tsx
+++ src/app/(site)/(home)/HomeHeroSection.tsx
@@ -1,5 +1,11 @@
 const HomeHeroSection = () => {
-  return <div>Hero Section</div>;
+  return <div className="h-screen flex items-center justify-center bg-gradient-to-b from-purple-200 to-blue-200">
+    <div className="text-center">
+      <h1 className="text-[#272C30] text-8xl font-bold leading-[142.113%]">TOGETHER!</h1>
+      <p className="text-[#272C30] font-normal leading-[142.113%]">To sustain a better future<br /> Creating endless opportunities for future generations!</p>
+      <button className="mt-9 inline-flex flex-col justify-center items-center gap-2.5 [background:rgba(228,235,255,0.50)] px-10 py-2 rounded-[20px] border-2 border-solid border-[#AD2CE8]">Explore More</button>
+    </div>
+  </div>;
 };
 
 export default HomeHeroSection;
