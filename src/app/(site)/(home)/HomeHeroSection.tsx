63571e8f2ef2f10327962feb504f70ead19a080a
trishane <trishanereid@gmail.com>
2024-09-27 13:12:01 +0530

    "MW-31 Updated with changes to typography, layout, and button styling."

--- src/app/(site)/(home)/HomeHeroSection.tsx
+++ src/app/(site)/(home)/HomeHeroSection.tsx
@@ -1,9 +1,15 @@
 const HomeHeroSection = () => {
   return <div className="h-screen flex items-center justify-center bg-gradient-to-b from-purple-200 to-blue-200">
     <div className="text-center">
-      <h1 className="text-[#272C30] text-8xl font-bold leading-[142.113%]">TOGETHER!</h1>
-      <p className="text-[#272C30] font-normal leading-[142.113%]">To sustain a better future<br /> Creating endless opportunities for future generations!</p>
-      <button className="mt-9 inline-flex flex-col justify-center items-center gap-2.5 [background:rgba(228,235,255,0.50)] px-10 py-2 rounded-[20px] border-2 border-solid border-[#AD2CE8]">Explore More</button>
+      <h1 className="text-[#272C30] font-bold leading-[142.113%] lg:text-8xl md:text-8xl sm:text-5xl">
+        TOGETHER!
+      </h1>
+      <p className="text-[#272C30] font-normal leading-[142.113%] font-2x1 md:text-base sm:text-xs mx-2 mt-3">
+        To sustain a better future<br /> Creating endless opportunities for future generations!
+      </p>
+      <button className="mt-10 inline-flex flex-col justify-center items-center gap-2.5 [background:rgba(228,235,255,0.50)] sm:px-5 sm:py-1 md:text-xs rounded-[20px] border-2 border-solid border-[#AD2CE8]">
+        Explore More
+      </button>
     </div>
   </div>;
 };
