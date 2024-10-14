5493985a8c347ef9811d02d9b91fa9e0f2c520f5
Vinula Senarathne <vinulapc@gmail.com>
2024-10-14 10:06:55 +0530

    fix: image warnings

--- src/app/(site)/(home)/HomeVideoSection.tsx
+++ src/app/(site)/(home)/HomeVideoSection.tsx
@@ -1,47 +1,39 @@
-import Image from 'next/image';
-import r2rImage from '../../images/video-section/background.jpg';
+import Image from "next/image";
+import r2rImage from "../../images/video-section/background.jpg";
 
 const HomeVideoSection = () => {
   return (
-    <div className='relative w-full h-80'>
-
+    <div className="relative w-full h-80">
       <Image
         src={r2rImage}
-        alt='no image'
-        layout="fill"
-        objectFit="cover"
-        className='opacity-40'
+        alt="no image"
+        fill
+        style={{ objectFit: "cover" }}
+        className="opacity-40"
+        priority
       />
 
-      <div className='absolute inset-0 flex items-center justify-center z-10 columns-2 p-10 gap-20 '>
-
-        <p className='text-[#6F2D8D] lg:text-4xl  md:text-3xl text-right font-poppins font-bold text-shadow-outline leading-8'>
-          WE <span className='text-black'>RECEIVED THE</span> UNITED<br />
-          NATIONS <span className='text-black'> SDG </span> ACTION<br />
-          AWARDS <span className='text-black'>IN </span> 2018
+      <div className="absolute inset-0 flex items-center justify-center z-10 columns-2 p-10 gap-20 ">
+        <p className="text-[#6F2D8D] lg:text-4xl  md:text-3xl text-right font-poppins font-bold text-shadow-outline leading-8">
+          WE <span className="text-black">RECEIVED THE</span> UNITED
+          <br />
+          NATIONS <span className="text-black"> SDG </span> ACTION
+          <br />
+          AWARDS <span className="text-black">IN </span> 2018
         </p>
 
-        <iframe className='h-80 hover:aspect-square w-2/4'
+        <iframe
+          className="h-80 hover:aspect-square w-2/4"
           src="https://www.youtube.com/embed/GkZgdGhLhwM"
           title="UN SDG Action Award 2018 - UN Global Festival of Action, Bonn, Germany."
           frameBorder="10"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
           referrerPolicy="strict-origin-when-cross-origin"
-          allowFullScreen>
-        </iframe>
+          allowFullScreen
+        ></iframe>
       </div>
-
-
-
-
-
     </div>
-
-
   );
 };
 
 export default HomeVideoSection;
-
-
-
