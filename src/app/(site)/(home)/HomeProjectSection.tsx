3e80f5f0e41f66610d431395a8edd527feca6ba9
Malindu Lakshan <malindulakshan3002@gmail.com>
2024-11-20 15:15:11 +0530

    button size ajustment

--- src/app/(site)/(home)/HomeProjectSection.tsx
+++ src/app/(site)/(home)/HomeProjectSection.tsx
@@ -18,18 +18,19 @@ const HomeProjectSection = () => {
   return (
     <section className="bg-[#EAF1FF] py-12">
       <div className="container mx-auto px-6 lg:px-8">
-        <div className="text-left pl-5 pt-5">
-          <span className="font-medium text-2xl lg:text-4xl">OUR</span>
-          <h1 className="text-4xl font-bold text-gray-900 mb-8">PROJECTS</h1>
+      <div className="text-center mb-8">
+        <h1 className="text-4xl font-bold text-gray-900">
+          <span className="font-medium text-2xl lg:text-4xl">OUR </span>PROJECTS
+        </h1>
         </div>
-        <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2">
+        <div className="grid gap-8 sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2  mx-4 lg:mx-8 max-w-[90%] sm:max-w-none">
           {projects.map((project) => (
             <div
               key={project.id}
-              className="bg-white rounded-lg shadow-lg overflow-hidden flex transform transition duration-300 hover:scale-105 hover:shadow-2xl"
+              className="bg-white rounded-lg shadow-lg overflow-hidden flex transform transition duration-300 hover:scale-105 hover:shadow-2xl w-[500px] mx-auto"
             >
-              
-              <div className="w-1/2">
+
+              <div className="w-1/2  h-50 sm:h-60">
                 <Image
                   src={project.imageSrc}
                   alt={project.title}
@@ -41,12 +42,12 @@ const HomeProjectSection = () => {
               <div className="p-6 flex flex-col justify-center w-1/2">
                 <h3 className="text-lg font-semibold text-black">{project.title}</h3>
                 <p className="text-sm text-gray-500 mt-2">{project.description}</p>
-                <a
-                  href="#"
-                  className="mt-4 px-4 py-2 text-white bg-pink-500 hover:bg-pink-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-opacity-50"
-                >
+
+
+                <button className="w-[100px] h-[25px] mt-9 px-4 py-2 text-[10px] text-white bg-pink-500 hover:bg-pink-600 rounded-lg focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-opacity-50 flex items-center justify-center">
                   View All News
-                </a>
+                </button>
+
               </div>
             </div>
           ))}
